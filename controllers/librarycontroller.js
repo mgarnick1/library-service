c3115e92d3f82d913429af2f518c86b0fd9bdfe5
Mark <mgarnick@techtonicgroup.com>
2018-08-03 09:16:27 -0600

    update search to find title, author, or publication date.

--- controllers/librarycontroller.js
+++ controllers/librarycontroller.js
@@ -81,14 +81,15 @@ router.post('/', function (req, res) {
 router.get('/search/:search', function(req,res) {
   // console.log("this happened!");
   // Library.collection.createIndex({Title: 'text'}, {Author: 'text'});
-  // console.log(req.params.search);
-  // Library.find({ $or: [{title: req.params.search},{author: }]}, function(){
+  let text = req.params.search, format = new RegExp(text), match = { $regex: format, $options: 'igx' }
+  let searchProps = [{Title: match}, {Author: match}, {Publish_Date: match}];
+  Library.find({ $or: searchProps}, function(err, book){
   //   if (err) return res.status(500).send("There was a problem adding the information to the database.");
   //   res.status(200).send(book);
   // });
 
-  Library.aggregate([{$match: {$text: {$search: req.params.search}}}],
-  function (err, book) {
+  // Library.aggregate([{$match: {$text: {$search: req.params.search}}}],
+  // function (err, book) {
     if (err) return res.status(500).send("There was a problem adding the information to the database.");
     res.status(200).send(book);
   })
